4ed38c8dda7e929ec1d8dfea536db28aa034bb49
esha mazhar <eshamazhar10@gmail.com>
2026-02-04 10:58:19 +0500

    Day-7 Ecommerce-project

---
+++ Day 7/Ecommerce-Project/src/App.jsx
@@ -0,0 +1,19 @@
+
+import { Routes, Route } from 'react-router';
+import { HomePage } from './Pages/HomePage'
+import './App.css'
+
+function App() {
+  
+  return (
+    <Routes>
+      <Route  path="/" element={  <HomePage /> } ></Route>
+      <Route  path="checkout" element={  <div> Test Page </div> } ></Route>
+
+
+    </Routes>
+ 
+  );
+}
+
+export default App
