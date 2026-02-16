b38b1c3a2e8817e1e9c0809eefb0e1b94f46fada
esha mazhar <eshamazhar10@gmail.com>
2026-02-16 13:00:31 +0500

    Ecommerce-Checkout-page

--- Day 7-9/Ecommerce-Project/src/App.jsx
+++ Day 7-9/Ecommerce-Project/src/App.jsx
@@ -1,14 +1,25 @@
 import { Routes, Route } from 'react-router-dom';
+import  axios  from 'axios';
+import { useState, useEffect } from 'react';
 import { HomePage } from './Pages/HomePage';
 import { CheckoutPage } from './Pages/CheckoutPage';
 import { Orders } from './Pages/Orders';
 import { Tracking } from './Pages/Tracking';
 import './App.css'
 function App() {
+   const [cart, setCart] = useState([]);
+
+   useEffect(()=>{
+     axios.get('/api/cart-items?expand=product')
+        .then((response) =>{
+          setCart(response.data);
+        });
+
+   }, []);
   return (
   <Routes>
-     <Route index element={<HomePage />} />
-    <Route path="checkout" element={<CheckoutPage />} />
+     <Route index element={<HomePage cart={cart} />} />
+    <Route path="checkout" element={<CheckoutPage cart={cart} />} />
     <Route path="orders" element={<Orders />} />
     <Route path="tracking" element={<Tracking />} />
      </Routes>
