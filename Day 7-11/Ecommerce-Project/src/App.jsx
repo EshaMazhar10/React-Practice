4ea04476830a4c3658a2e32257278a0f0a1f9c24
esha mazhar <eshamazhar10@gmail.com>
2026-02-17 22:59:55 +0500

    Ecommerce-Manage page

--- Day 7-11/Ecommerce-Project/src/App.jsx
+++ Day 7-11/Ecommerce-Project/src/App.jsx
@@ -1,26 +1,30 @@
 import { Routes, Route } from 'react-router-dom';
 import  axios  from 'axios';
 import { useState, useEffect } from 'react';
-import { HomePage } from './Pages/HomePage';
-import { CheckoutPage } from './Pages/CheckoutPage';
-import { Orders } from './Pages/Orders';
+import { HomePage } from './Pages/home/HomePage';
+import { CheckoutPage } from './Pages/Checkout/CheckoutPage';
+import { OrderPage } from './Pages/Orders/OrderPage';
 import { Tracking } from './Pages/Tracking';
 import './App.css'
 function App() {
    const [cart, setCart] = useState([]);
 
    useEffect(()=>{
-     axios.get('/api/cart-items?expand=product')
-        .then((response) =>{
-          setCart(response.data);
-        });
+    const fetchAppData = async() => {
+      const response = await axios.get('/api/cart-items?expand=product')
+        setCart(response.data);
+       
 
+
+    };
+    fetchAppData();
+    
    }, []);
   return (
   <Routes>
      <Route index element={<HomePage cart={cart} />} />
     <Route path="checkout" element={<CheckoutPage cart={cart} />} />
-    <Route path="orders" element={<Orders />} />
+    <Route path="orders" element={<OrderPage cart={cart} />} />
     <Route path="tracking" element={<Tracking />} />
      </Routes>
   );
